64d7ff9c681c1adc75ba31782726703caf79b72b
Alexander Yee <alexanderyee@email.arizona.edu>
2015-12-05 11:20:21 -0700

    Added functions to model.php

--- model.php
+++ model.php
@@ -42,9 +42,9 @@ class Model {
 		// | 4 | Guardians of the Galaxy | edg | Crazy | rotten |
 		// +----+-------------------------+------------+-----------------------------------+--------+
 		$stmt = $this->DB->prepare (
-				"SELECT Reviews.rating, Reviews.review, Reviewers.name, publication
-				 FROM Reviews
-				 INNER JOIN Reviewers
+				"SELECT Reviews.rating, Reviews.review, Reviewers.name, Reviewers.publication
+				 FROM Reviewers
+				 INNER JOIN Reviews
 				 ON Reviewers.reviewerID=Reviews.reviewerID
 				 WHERE title = :title" );
 		$stmt->bindParam ( 'title', $title );
@@ -53,10 +53,50 @@ class Model {
 		return $array;
 	}
 
-  public function addNewMovie($title){
+  public function addNewMovie($title, $filename, $director, $mpaa, $score, $year, $runtime, $boxOffice){
+  	$stmt = $this->DB->prepare (
+				"INSERT INTO titles(title, imageFileName, director, mpaaRating, score, year, runtime, boxOffice) 
+				VALUES(:title, :filename, :director, :mpaa, :score, :year, :runtime, :boxOffice); ");
+		$stmt->bindParam ( 'title', $title );
+		$stmt->bindParam ( 'filename', $filename );
+		$stmt->bindParam ( 'director', $director );
+		$stmt->bindParam ( 'mpaa', $mpaa );
+		$stmt->bindParam ( 'score', $score );
+		$stmt->bindParam ( 'year', $year );
+		$stmt->bindParam ( 'runtime', $runtime );
+		$stmt->bindParam ( 'boxOffice', $boxOffice );
+		$stmt->execute ();		
   }
-  public function addReview($title){
+  public function addReview($title, $ID, $review, $rating){
+  	$stmt = $this->DB->prepare (
+				"INSERT INTO reviews(title, reviewerID, review, rating) 
+				VALUES(:title, :ID, :review, :rating); ");
+		$stmt->bindParam ( 'title', $title );
+		$stmt->bindParam ( 'ID', $ID );
+		$stmt->bindParam ( 'review', $review );
+		$stmt->bindParam ( 'rating', $rating );
+		$stmt->execute ();
   }
-
-}
+  public function getOverallInfoFor($title){ 
+  	$stmt = $this->DB->prepare (
+				"SELECT imageFileName, director, mpaaRating, score, year, runtime, boxOffice
+				 FROM titles
+				 WHERE title = :title" );
+		$stmt->bindParam ( 'title', $title );
+		$stmt->execute ();
+		$array = $stmt->fetchAll ( PDO::FETCH_ASSOC );
+		return $array;
+  }
+  public function exists($title){ 
+  // search key in titles, returns true if exists
+  	$stmt = $this->DB->prepare ( 'SELECT * FROM titles WHERE title = :title' );
+			$stmt->bindParam ( 'title', $title );
+			$stmt->execute ();
+			$stmt->fetch ();
+			
+			if ($stmt->rowCount () === 0)
+				return TRUE;
+			else
+				return FALSE;
+} 
 ?>
